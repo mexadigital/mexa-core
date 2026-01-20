ca749f96cf9bb8da382165728da53070f22dfb0c
mexadigital <cagj78@gmail.com>
2026-01-19 20:32:50 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from app.api.inventory.router import router as inventory_router
 
-app = FastAPI(title="Mexa.Digital Core")
+app = FastAPI(
+    title="Mexa.Digital Core",
+    version="0.1.0"
+)
+
+app.include_router(inventory_router)
 
 @app.get("/health")
 def health():
