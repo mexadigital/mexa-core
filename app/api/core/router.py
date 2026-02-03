8b6561703436ea7801187df8b7f05cea70b2e815
mexadigital <cagj78@gmail.com>
2026-02-02 21:03:50 -0600

    Update router.py

--- app/api/core/router.py
+++ app/api/core/router.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
+from .empresas import router as empresas_router
 
 router = APIRouter(prefix="/core", tags=["core"])
+
+router.include_router(
+    empresas_router,
+    prefix="/empresas",
+    tags=["core-empresas"]
+)
