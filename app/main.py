249b7d56070be8389cbfea8b7e8ccb149ffbf93e
mexadigital <cagj78@gmail.com>
2026-02-02 20:25:45 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+
+from app.db import init_db
 from app.api.inventory.router import router as inventory_router
 
 app = FastAPI(
     title="Mexa.Digital Core",
     version="0.1.0"
 )
+
+# 🔑 CREA DB Y TABLAS AL ARRANCAR
+@app.on_event("startup")
+def startup():
+    init_db()
 
 app.include_router(inventory_router)
 
