e9d5368654b0337d51ccd165b1d6c17b8728c66f
mexadigital <cagj78@gmail.com>
2026-03-14 18:06:04 -0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,25 +12,26 @@ from app.api.productos import router as productos_router
 from app.api.movimientos.router.router import router as movimientos_router
 from app.api.organizaciones.router import router as organizaciones_router
 from app.api.inventario import router as inventario_router
+from app.api.auth import router as auth_router
 
-# Crear app
 app = FastAPI(title=settings.APP_NAME)
 
 # Crear tablas
 Base.metadata.create_all(bind=engine)
 
-# Incluir routers
+# Routers
 app.include_router(productos_router)
 app.include_router(movimientos_router)
 app.include_router(organizaciones_router)
 app.include_router(inventario_router)
+app.include_router(auth_router)
 
-# Ruta raíz
+# Rutas base
 @app.get("/")
 def root():
     return {"message": "Mexa Core funcionando 🚀"}
 
-# Health check
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
