34bb401901c8b04f2a7b75ebdde62e71a7c9c298
mexadigital <cagj78@gmail.com>
2026-03-14 18:50:07 -0600

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from app.core.config import settings
 
-# Engine (SQLite)
+# Engine
 engine = create_engine(
     settings.DATABASE_URL,
     connect_args={"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {},
