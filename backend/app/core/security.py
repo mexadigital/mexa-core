e40ac4487969c80fd30eff595e290fc1395bcbe6
mexadigital <cagj78@gmail.com>
2026-03-21 19:49:00 -0600

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -2,8 +2,8 @@ from datetime import datetime, timedelta, timezone
 from jose import jwt
 from passlib.context import CryptContext
 
-SECRET_KEY = "mexa_super_secret_key_cambiar_en_produccion"
-ALGORITHM = "HS256"
+from app.core.config import settings
+
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -23,4 +23,8 @@ def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
