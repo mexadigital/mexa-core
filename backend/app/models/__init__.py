1f051138cbc75da23c0bb2ca256da84a5f35cf9e
mexadigital <cagj78@gmail.com>
2026-03-30 19:09:56 -0600

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,9 +1,6 @@
+from app.models.organizacion import Organizacion
 from app.models.producto import Producto
 from app.models.movimiento import Movimiento
-from app.models.organizacion import Organizacion
-
-# 👇 ESTE ES EL QUE TE FALTA
+from app.models.usuario import Usuario
 from app.models.ubicacion import Ubicacion
-
-# 👇 y también inventario si ya lo hiciste
 from app.models.inventario import Inventario
