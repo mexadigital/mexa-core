9dc1fba5f22a81cd7b5494dcea56aafd9c406dcd
mexadigital <cagj78@gmail.com>
2026-04-09 18:52:24 -0600

    Update movimiento.py

--- backend/app/schemas/movimiento.py
+++ backend/app/schemas/movimiento.py
@@ -1,17 +1,14 @@
-
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Literal
 
 
-# 🔹 SOLO lo que el usuario debe enviar
 class MovimientoCreate(BaseModel):
     producto_id: int
     tipo: Literal["entrada", "salida"]
     cantidad: int
 
 
-# 🔹 Lo que regresa el sistema
 class MovimientoOut(BaseModel):
     id: int
     organizacion_id: int
