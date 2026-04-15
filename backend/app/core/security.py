b860bfcae4e5eef4bd057899f3d1112f88d4317a
mexadigital <cagj78@gmail.com>
2026-04-14 20:24:18 -0600

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,24 +4,22 @@ from passlib.context import CryptContext
 
 from app.core.config import settings
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
-    password = password[:72]
     return pwd_context.hash(password)
 
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    plain_password = plain_password[:72]
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(password, hashed_password)
 
 
-def create_access_token(data: dict):
+def create_access_token(data: dict) -> str:
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
     to_encode.update({"exp": expire})
     return jwt.encode(
         to_encode,
