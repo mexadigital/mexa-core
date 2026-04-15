43a79420627654f5ca7a11dc72a60984d76ec480
mexadigital <cagj78@gmail.com>
2026-04-14 19:20:21 -0600

    Update usuario.py

--- backend/app/models/usuario.py
+++ backend/app/models/usuario.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship
 
 from app.db.base import Base
 
@@ -9,8 +9,14 @@ class Usuario(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     organizacion_id = Column(Integer, ForeignKey("organizaciones.id"), nullable=False)
+
     nombre = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    activo = Column(String, default="si")
+
+    rol = Column(String, nullable=False, default="usuario")
+    activo = Column(String, nullable=False, default="si")
+
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    organizacion = relationship("Organizacion")
